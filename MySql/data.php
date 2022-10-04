42b0a9b02ce088700b29d06d68ed69df856d9202
Prajwal Kadam <81584701+imPrajwalKadam@users.noreply.github.com>
2022-10-04 18:59:12 +0530

    Update data.php

--- MySql/data.php
+++ MySql/data.php
@@ -4,8 +4,7 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "ip_address";
-
-
+echo $ipadress;
 //Create a connection object
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 
@@ -27,16 +26,34 @@ if($result) {
   } else {
     echo "Error: " . $query . "<br>" . $conn->error;
   }
-  $getData = "SELECT * FROM ip_count";
-  $result1 =  mysqli_query($conn,$getData);
-  if($getData) {
-    echo "New data is fetch successfully";
-  } else {
-    echo "Error: " . $getData . "<br>" . $conn->error;
+  
+  $query2 = "SELECT * FROM ip_count";
+  $result2 = mysqli_query($conn,$query2);
+ $arr=[];
+  foreach($result2 as $value)
+  {
+    $arr['ipaddress'][]=$value['ip_address'];
+  }
+  var_dump($arr);
+  $iCnt = 0;
+  $i=0;
+  for($i = 0; $i < count($arr);$i++)
+  {
+    if($arr[$i]==$ipadress)
+    {
+        $iCnt++;
+    }
   }
 
+  
+// While a row of data exists, put that row in $row as an associative array
+// Note: If you're expecting just one row, no need to use a loop
+// Note: If you put extract($row); inside the following loop, you'll
+//       then create $userid, $fullname, and $userstatus
+// while ($row = mysql_fetch_assoc($result1)) {
+//     echo $row["ip_address"];
+// }
 
-print("data is".$result);
 // $result1;
   
 
