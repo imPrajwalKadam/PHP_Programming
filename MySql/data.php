26617167021f53255b6daaeb1ced316a19a70b82
Prajwal Kadam <81584701+imPrajwalKadam@users.noreply.github.com>
2022-10-06 18:51:27 +0530

    Update data.php

--- MySql/data.php
+++ MySql/data.php
@@ -72,3 +72,57 @@ if($result) {
 //     }
   // }
 ?>
+<?php
+$ipadress=$_POST['ipadress'];
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "ip_address";
+// echo $ipadress;
+//Create a connection object
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+$tempIP = $ipadress;
+  // $query2 = "SELECT * FROM ip_count";
+  // $result2 = mysqli_query($conn,$query2);
+  //  $arr=[];
+  // $query3 = "";
+  // foreach($result2 as $value)
+  // {
+  //   // echo $result;
+  //   array_push($arr,$value['ip_address']);
+  // }
+  // if(count($arr)>0)
+  // {
+  // var_dump(count($arr));
+  // for($i = 0; $i < count($arr);$i++)
+  // {
+      // echo "total".$arr[$i];
+      $query4 = "INSERT INTO ip_count(ip_address) VALUES ('".$ipadress."') ON DUPLICATE KEY UPDATE ip_Count = ip_Count + 1";
+      $result2 = mysqli_query($conn,$query4);
+
+  $query2 = "SELECT * FROM ip_count";
+  $result2 = mysqli_query($conn,$query2);
+   $arr=[];
+   $i = 0;
+  foreach($result2 as $value)
+  {
+    // echo $result;
+    array_push($arr,$value['ip_Count']);
+  }
+  var_dump($arr);
+  echo $arr['ip_Count'];
+  if($arr['ip_Count']==5)
+  {
+    return true;
+  }else{
+    return false;
+  }
+
+  // }
+// }
+// else
+// {
+  // $query = "INSERT INTO ip_count(ip_address)VALUES('".$ipadress."')";
+  // $result2 = mysqli_query($conn,$query);
+// }
+?>
