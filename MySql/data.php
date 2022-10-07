28908e711e73bf70a2c031863ffd28754a42c961
Prajwal Kadam <81584701+imPrajwalKadam@users.noreply.github.com>
2022-10-07 14:08:30 +0530

    Update data.php

--- MySql/data.php
+++ MySql/data.php
@@ -1,128 +1,29 @@
-<?php
-$ipadress=$_POST['ipadress'];
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "MYDB1_ip";
-echo $ipadress;
-//Create a connection object
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-$tempIP = $ipadress;
-//terminate if connection is not valid
-if(!$conn)
-{
-     die("connection failed ". mysqli_connect_error());
-}
-else{
-echo"connection is successful<br>";
-}
+  <?php
+  $ipaddress=$_POST['ipaddress'];
+  $servername = "localhost";
+  $username = "root";
+  $password = "";
+  $dbname = "mydb1_ip";
+  $conn = mysqli_connect($servername, $username, $password, $dbname);
+  
 
-$query = "INSERT INTO ip_count(ip_address)VALUES('".$ipadress."')";
-
-$result = mysqli_query($conn,$query);
-
-if($result) {
-    echo "New record created successfully";
-  } else {
-    echo "Error: " . $query . "<br>" . $conn->error;
-  }
-
-  $query2 = "SELECT * FROM ip_count";
-  $result2 = mysqli_query($conn,$query2);
-  // $arr=[];
-  $i=0;
-  while($arr = mysqli_fetch_array($result2, MYSQLI_ASSOC)){
-    print("ID: ".$arr["ip_address"]."\n");
-    if(count($arr)<=5)
+  //script for storing new ip and update duplicate ip's count
+        $query4 = "INSERT INTO ip_count(ip_address) VALUES ('".$ipaddress."') ON DUPLICATE KEY UPDATE ip_Count = ip_Count + 1";
+        $result2 = mysqli_query($conn,$query4);
+      
+    //script of compairing total count of ip if ip_count==5 script displays form else continue..
+    $query2 = "SELECT * FROM ip_count";
+    $result2 = mysqli_query($conn,$query2);
+    $arr=[];
+    $i = 0;
+    $rows = [];
+    while($row = mysqli_fetch_array($result2))
     {
-      $i++;
-    }
-    echo "<bt>".$i. "<br>";
-    echo $tempIP."<-temp>";
-    if(($arr[$i]==$tempIP)&&(count($arr)<=5))
+    if($row['ip_Count']==5)
     {
-      echo"array count is:". count($arr)."/br";
-      echo "enjoy our free services";
+      print "please fill the form";
     }
     else{
-      echo "please fill the above form";
+      print"continue with our services ..";
     }
-  }
-  // echo "<br>".count($arr);
-  // foreach($result2 as $value)
-  // {
-  //   echo $result;
-  //   $arr['ipaddress'][]=$value['ip_address'];
-  // }
-  // var_dump($arr);
-  // $iCnt = 0;
-  // for($iCnt = 0; $iCnt < count($arr);$iCnt++)
-  // {
-    //  echo "array length is".count($arr)."\n";
-    //  echo $iCnt;
-          // echo $arr[$i]."<br />";
-//     if(($arr[$i]==$tempIP))
-//     {
-//         $iCnt++;
-//         echo "count is : ".$iCnt;
-//     }
-//     if($iCnt <= 5)
-//     {
-//           break;
-//     }
-  // }
-?>
-<?php
-$ipadress=$_POST['ipadress'];
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "ip_address";
-// echo $ipadress;
-//Create a connection object
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-$tempIP = $ipadress;
-  // $query2 = "SELECT * FROM ip_count";
-  // $result2 = mysqli_query($conn,$query2);
-  //  $arr=[];
-  // $query3 = "";
-  // foreach($result2 as $value)
-  // {
-  //   // echo $result;
-  //   array_push($arr,$value['ip_address']);
-  // }
-  // if(count($arr)>0)
-  // {
-  // var_dump(count($arr));
-  // for($i = 0; $i < count($arr);$i++)
-  // {
-      // echo "total".$arr[$i];
-      $query4 = "INSERT INTO ip_count(ip_address) VALUES ('".$ipadress."') ON DUPLICATE KEY UPDATE ip_Count = ip_Count + 1";
-      $result2 = mysqli_query($conn,$query4);
-
-  $query2 = "SELECT * FROM ip_count";
-  $result2 = mysqli_query($conn,$query2);
-   $arr=[];
-   $i = 0;
-  foreach($result2 as $value)
-  {
-    // echo $result;
-    array_push($arr,$value['ip_Count']);
-  }
-  var_dump($arr);
-  echo $arr['ip_Count'];
-  if($arr['ip_Count']==5)
-  {
-    return true;
-  }else{
-    return false;
-  }
-
-  // }
-// }
-// else
-// {
-  // $query = "INSERT INTO ip_count(ip_address)VALUES('".$ipadress."')";
-  // $result2 = mysqli_query($conn,$query);
-// }
-?>
+    }
