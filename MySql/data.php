21192609e34a11bf8e5d46ae75f4a16219922d9e
Prajwal Kadam <81584701+imPrajwalKadam@users.noreply.github.com>
2022-10-11 19:46:13 +0530

    Update data.php

--- MySql/data.php
+++ MySql/data.php
@@ -1,29 +1,34 @@
-  <?php
+    <?php
   $ipaddress=$_POST['ipaddress'];
   $servername = "localhost";
   $username = "root";
   $password = "";
   $dbname = "mydb1_ip";
   $conn = mysqli_connect($servername, $username, $password, $dbname);
-  
-
+  echo $ipaddress;  
   //script for storing new ip and update duplicate ip's count
         $query4 = "INSERT INTO ip_count(ip_address) VALUES ('".$ipaddress."') ON DUPLICATE KEY UPDATE ip_Count = ip_Count + 1";
         $result2 = mysqli_query($conn,$query4);
-      
     //script of compairing total count of ip if ip_count==5 script displays form else continue..
-    $query2 = "SELECT * FROM ip_count";
+    $query2 = "SELECT * FROM ip_count where ip_address = '$ipaddress'";
     $result2 = mysqli_query($conn,$query2);
     $arr=[];
     $i = 0;
     $rows = [];
     while($row = mysqli_fetch_array($result2))
     {
-    if($row['ip_Count']==5)
+    if($row['ip_Count'] == 4)
     {
+      // echo '<script>alert("Inside IF BLOCK")</script>';
+
       print "please fill the form";
     }
     else{
+      // echo '<script>alert("Inside ELSE BLOCK")</script>';
       print"continue with our services ..";
     }
     }
+
+    ?>
+
+
