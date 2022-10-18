ed6dd040d61956f63f3a3bf2e86e0452b0b1b1a2
Prajwal Kadam <81584701+imPrajwalKadam@users.noreply.github.com>
2022-10-19 01:08:07 +0530

    Update data.php

--- MySql/data.php
+++ MySql/data.php
@@ -1,34 +1,25 @@
 <?php
-  if(isset($_POST['ipaddress']))
-  {
-    $ipaddress=$_POST['ipaddress'];
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "mydb1_ip";
-    $conn = mysqli_connect($servername, $username, $password, $dbname);
-     //script for storing new ip and update duplicate ip's count
-     $query4 = "INSERT INTO user_ip(ip_address)VALUES('".$ipaddress."') ON DUPLICATE KEY UPDATE ip_Count = ip_Count + 1";
-     $result2 = mysqli_query($conn,$query4);
- //script of compairing total count of ip if ip_count==5 script displays form else continue..
- $query2 = "SELECT * FROM user_ip where ip_address = '$ipaddress'";
- $result2 = mysqli_query($conn,$query2);
- $arr=[];
- $i = 0;
- $rows = [];
- while($row = mysqli_fetch_array($result2))
- {
- if((($row['ip_Count'] >= 0)&&($row['is_reg'])==1))
- {
-  echo'success';die();
- }
-// else if($row['mobile'] == null && ()){
-// }
- else{
-  echo "failed";die();
- }//end else
- }//end while
-  }
+if (isset($_POST['ipaddress'])) {
+  $ipaddress = $_POST['ipaddress'];
+  $servername = "localhost";
+  $username = "root";
+  $password = "";
+  $dbname = "mydb1_ip";
+  $conn = mysqli_connect($servername, $username, $password, $dbname);
+  //script for storing new ip and update duplicate ip's count
+  $query4 = "INSERT INTO ip_count(ip_address)VALUES('" . $ipaddress . "') ON DUPLICATE KEY UPDATE ip_Count = ip_Count + 1";
+  $result2 = mysqli_query($conn, $query4);
+  //script of compairing total count of ip if ip_count==5 script displays form else continue..
+  $query2 = "SELECT * FROM ip_count where ip_address = '$ipaddress'";
+  $result2 = mysqli_query($conn, $query2);
+  while ($row = mysqli_fetch_array($result2)) {
+    if (($row['ip_Count']>= 4)&&($row['is_reg'] == 0)) {
+      echo 'success';
+      die();
+    } else {
+      echo "failed";
+      die();
+    }
+  } //end while
+}
 ?>
-
-
